f81e936e0827e6d99967d49f67376acfb1f13c0b
kulapoo <kulaskulapoo@gmail.com>
2025-04-06 15:08:53 +0800

    update

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,24 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, PathArguments, Type, TypePath};
+
+
+fn is_option_type(ty: &Type) -> bool {
+    // First, check if it's a Type::Path
+    if let Type::Path(TypePath { qself: None, path }) = ty {
+        // Then check if the path has at least one segment
+        if let Some(segment) = path.segments.first() {
+            // Check if the first segment is "Option"
+            if segment.ident == "Option" {
+                // Finally, check if it has angle-bracketed arguments
+                if let PathArguments::AngleBracketed(_) = &segment.arguments {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -18,12 +36,23 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let field_name = &field.ident;
         let field_type = &field.ty;
 
-        quote! {
-            pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
-                self.#field_name = Some(#field_name);
-                self
+
+        if is_option_type(field_type) {
+            quote! {
+                pub fn #field_name(&mut self, #field_name: Option<#field_type>) -> &mut Self {
+                    self.#field_name = Some(#field_name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+                    self.#field_name = Some(#field_name);
+                    self
+                }
             }
         }
+
     });
 
     let field_defs = fields.iter().map(|field| {
@@ -48,10 +77,20 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         let field_checks = fields.iter().map(|field| {
             let field_name = &field.ident;
-            quote! {
-                let #field_name = self.#field_name.take().ok_or_else(||
-                    format!("{} is not set", stringify!(#field_name)))?;
+            let field_type = &field.ty;
+
+            if is_option_type(field_type) {
+                quote! {
+                    let #field_name = self.#field_name.clone().flatten().take();
+                }
+            } else {
+                quote! {
+                    let #field_name = self.#field_name.take().ok_or_else(||
+                        format!("{} is not set", stringify!(#field_name)))?;
+                }
             }
+
+
         });
 
         let field_inits = fields.iter().map(|field| {
